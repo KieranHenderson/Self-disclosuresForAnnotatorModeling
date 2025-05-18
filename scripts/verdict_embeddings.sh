944adcb08a469d937518b9462bcd50d597139ec3
Kian Omoomi <59165380+kianomoomi@users.noreply.github.com>
2025-05-18 20:36:23 +0330

    Add verdict_embeddings.sh file

---
+++ scripts/verdict_embeddings.sh
@@ -0,0 +1,8 @@
+python src/verdict_embeddings.py \
+--comment_embeddings_path='data/embeddings/precomputed_comment_embeddings_postlevel.pkl' \
+--post_embeddings_path='data/embeddings/precomputed_post_embeddings_postlevel.pkl' \
+--path_to_data='data/' \
+--output_dir='data/final_embeddings/' \
+--top_k=5 \
+--embed_sentences=false \
+--output_file_name='verdict_embeddings_postlevel_5'
