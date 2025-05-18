54f788b9d12e292e2d877f2ff630b870b174b984
Kian Omoomi <59165380+kianomoomi@users.noreply.github.com>
2025-05-18 19:40:51 +0330

    Add precompute_comment_embeddings.sh file

---
+++ scripts/precompute_comment_embeddings.sh
@@ -0,0 +1,8 @@
+# This script should be run once with --embed_sentences=false and once with --embed_sentences=true
+# Adjust --output_file_name accordingly
+python ./src/precompute_comment_embeddings.py \
+--path_to_data='data/' \
+--dirname='data/amit_filtered_history/' \
+--output_dir='data/embeddings/' \
+--embed_sentences=false \
+--output_file_name='precomputed_comment_embeddings_postlevel' \
